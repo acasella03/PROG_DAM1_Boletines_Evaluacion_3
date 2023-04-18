74f9ddfc94be3acddbed1163d8383890d294c673
Angi Marliu Casella <acasella@danielcastelao.org>
2023-04-18 10:22:54 +0200

    Boletín 30

---
+++ PROG_Boletin30/src/prog_boletin30/PROG_Boletin30.java
@@ -0,0 +1,10 @@
+package prog_boletin30;
+
+public class PROG_Boletin30 {
+
+    public static void main(String[] args) {
+        Ventana ventana=new Ventana();
+        ventana.setVisible(true);
+    }
+    
+}
