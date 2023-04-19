4d81c2511d8c5eebe85be71f5bacb20c59933ad0
Angi Marliu Casella <acasella@danielcastelao.org>
2023-04-19 11:39:18 +0200

    Boletín 31

---
+++ PROG_Boletin31/src/prog_boletin31/PROG_Boletin31.java
@@ -0,0 +1,10 @@
+package prog_boletin31;
+
+public class PROG_Boletin31 {
+
+    public static void main(String[] args) {
+        Calculadora calcu=new Calculadora();
+        calcu.setVisible(true);
+    }
+    
+}
