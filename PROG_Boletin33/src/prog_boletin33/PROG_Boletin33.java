837b55278b00fdd6a19f2e49eeda6741f2210f9f
Angi Marliu Casella <acasella@danielcastelao.org>
2023-04-25 13:54:49 +0200

    Boletin 33

---
+++ PROG_Boletin33/src/prog_boletin33/PROG_Boletin33.java
@@ -0,0 +1,12 @@
+package prog_boletin33;
+
+import java.util.ArrayList;
+
+public class PROG_Boletin33 {
+
+    public static void main(String[] args) {
+        ArrayList<SeleccionFutbol> listaFutbol = new ArrayList<>();
+        
+    }
+
+}
