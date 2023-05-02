8f1607017124bed40ebc5e5f778473a599f9db4c
Angi Marliu Casella <acasella@danielcastelao.org>
2023-05-02 12:19:55 +0200

    Boletin 34

--- PROG_Boletin34/src/prog_boletin34/presentacion/PROG_Boletin34.java
+++ PROG_Boletin34/src/prog_boletin34/presentacion/PROG_Boletin34.java
@@ -1,9 +1,15 @@
 package prog_boletin34.presentacion;
 
+import java.util.ArrayList;
+import prog_boletin34.datos.ListaEmpleados;
+import prog_boletin34.entidades.Empleado;
+
 public class PROG_Boletin34 {
     
     public static void main(String[] args) {
-        
+        ListaEmpleados empleados=new ListaEmpleados();
+        ArrayList<Empleado> lista = null;
+        System.out.println(empleados.agregarEmpleado(lista));
     }
     
 }
