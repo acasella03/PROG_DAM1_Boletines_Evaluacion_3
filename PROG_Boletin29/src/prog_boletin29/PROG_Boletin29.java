e3a92bae49109afa07e2ff4535f923d7f46ce22e
Angi Marliu Casella <acasella@danielcastelao.org>
2023-05-08 09:25:09 +0200

    Boletin 29

--- PROG_Boletin29/src/prog_boletin29/PROG_Boletin29.java
+++ PROG_Boletin29/src/prog_boletin29/PROG_Boletin29.java
@@ -3,7 +3,11 @@ package prog_boletin29;
 public class PROG_Boletin29 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        DevolucionCliente ventana=new DevolucionCliente();
+        ventana.setVisible(true);
+        
+        NewUser ventana1=new NewUser();
+        ventana1.setVisible(true);
     }
     
 }
